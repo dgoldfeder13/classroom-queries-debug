907ace587120a242690af4ea4a23d08c1b5faeb8
dgoldfeder13 <114775261+dgoldfeder13@users.noreply.github.com>
2025-02-25 09:48:01 +0000

    enroll student doesnt work

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   #------------------------------
   # Department routes
 
-  get("/departments", { :controller => "department", :action => "index" })
+  get("/departments", { :controller => "departments", :action => "index" })
   
   get("/departments/:path_id", { :controller => "departments", :action => "show" })
 
@@ -23,11 +23,11 @@ Rails.application.routes.draw do
   
   get("/courses/:path_id", { :controller => "courses", :action => "show" })
 
-  get("/insert_course", { :controller => "courses", :action => "create" })
+  post("/insert_course", { :controller => "courses", :action => "create" })
 
   get("/courses", { :controller => "courses_controller", :action => "index" })
   
-  get("/modify_course/:path_id", { :controller => "courses", :action => "update" })
+  post("/modify_course/:path_id", { :controller => "courses", :action => "update" })
 
   get("/delete_course/:path_id", { :controller => "courses", :action => "destroy" })
 
@@ -39,8 +39,6 @@ Rails.application.routes.draw do
   get("/students/:path_id", { :controller => "students", :action => "show" })
 
   post("/insert_student", { :controller => "students", :action => "create" })
-
-  get("/students", { :controller => "students", :action => "index" })
   
   post("/modify_student/:path_id", { :controller => "students", :action => "update" })
 
@@ -48,6 +46,16 @@ Rails.application.routes.draw do
 
   #------------------------------
 
+  get("/enrollments", { :controller => "enrollments", :action => "index" })
+  
+  get("/enrollments/:path_id", { :controller => "enrollments", :action => "show" })
+
   post("/insert_enrollment", { :controller => "enrollments", :action => "create" })
+  
+  post("/modify_enrollment/:path_id", { :controller => "enrollments", :action => "update" })
+
+  get("/delete_enrollment/:path_id", { :controller => "enrollments", :action => "destroy" })
+
+  
 
 end
